df4b1c4a3af9cf87c3f3f6005360faf96d960f6d
Arjun8506 <140004161+Arjun8506@users.noreply.github.com>
2024-02-17 16:03:05 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
@@ -13,6 +14,8 @@ dotenv.config()
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve()
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -21,10 +24,12 @@ app.use("/api/auth", authroutes);
 app.use("/api/messages", messageroutes);
 app.use("/api/users", userroutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
-app.get("/", function(req, res){
-    res.send("main page")
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 })
+
 
 server.listen(PORT, () => {
     connectToDatabase()
